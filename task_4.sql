49e46b5050726a7d3492d847097afc16ba5ee260
Willy Peter <nguyamuwilly@gmail.com>
2025-08-17 14:59:56 +0300

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- Select the correct database
+USE alx_book_store;
+
+-- Print full description of the 'Books' table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
